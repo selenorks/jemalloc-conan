6d11adef1329acad2cb81fea95aa3d3be5f10a54
selenorks <fimochkin@gmail.com>
2017-01-17 20:27:24 +0300

    template for conan tools

--- build.py
+++ build.py
@@ -1,43 +1,11 @@
-import os
-import platform
-import sys
+from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    os.system('conan export selenorks/stable')
-   
-    def test(settings):
-        argv =  " ".join(sys.argv[1:])
-        command = "conan test_package %s %s" % (settings, argv)
-        retcode = os.system(command)
-        if retcode != 0:
-            exit("Error while executing:\n\t %s" % command)
-
-
-    if platform.system() == "Windows":
-        for compiler_version in ("14",):
-            compiler = '-s compiler="Visual Studio" -s compiler.version=%s ' % compiler_version
-            # Static x86
-            test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd')
-            test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD')
+    builder = ConanMultiPackager(username="myuser")
+    compiler = {"compiler": "Visual Studio", "compiler.version": "14"}
+    builder.add({"arch": "x86", "build_type": "Release", "compiler.runtime": "MDd"}.update(compiler))
+    builder.add({"arch": "x86", "build_type": "Debug", "compiler.runtime": "MD"}.update(compiler))
+    builder.add({"arch": "x86_64", "build_type": "Release", "compiler.runtime": "MDd"}.update(compiler))
+    builder.add({"arch": "x86_64", "build_type": "Debug", "compiler.runtime": "MD"}.update(compiler))
     
-            # Static x86_64
-            test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd')
-            test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD')
-
-    else:  # Compiler and version not specified, please set it in your home/.conan/conan.conf (Valid for Macos and Linux)
-        if not os.getenv("TRAVIS", False):  
-            # Static x86
-            test('-s arch=x86 -s build_type=Debug')
-            test('-s arch=x86 -s build_type=Release')
-    
-            # Shared x86
-            test('-s arch=x86 -s build_type=Debug')
-            test('-s arch=x86 -s build_type=Release')
-
-        # Static x86_64
-        test('-s arch=x86_64 -s build_type=Debug')
-        test('-s arch=x86_64 -s build_type=Release')
-
-        # Shared x86_64
-        test('-s arch=x86_64 -s build_type=Debug')
-        test('-s arch=x86_64 -s build_type=Release')
+    builder.run()
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,13 +2,20 @@ from conans.model.conan_file import ConanFile
 from conans import CMake
 import os
 
+############### CONFIGURE THESE VALUES ##################
+default_user = "selenorks"
+default_channel = "testing"
+#########################################################
+
+channel = os.getenv("CONAN_CHANNEL", default_channel)
+username = os.getenv("CONAN_USERNAME", default_user)
 
 class DefaultNameConan(ConanFile):
     name = "DefaultName"
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
-    requires = "jemalloc/4.3.1@selenorks/stable"
+    requires = "jemalloc/4.3.1@%s/%s" % (username, channel)
 
     def build(self):
         cmake = CMake(self.settings)
