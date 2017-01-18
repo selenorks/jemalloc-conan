07da27ba1cab5f79c20ebd0cd3659a18a8f965ab
selenorks <fimochkin@gmail.com>
2017-01-18 15:44:13 +0300

    fixed incorrect runtime

--- build.py
+++ build.py
@@ -3,9 +3,9 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="selenorks")
     compiler = {"compiler": "Visual Studio", "compiler.version": "14"}
-    builder.add(dict({"arch": "x86", "build_type": "Release", "compiler.runtime": "MDd"}.items() + compiler.items()))
-    builder.add(dict({"arch": "x86", "build_type": "Debug", "compiler.runtime": "MD"}.items() + compiler.items()))
-    builder.add(dict({"arch": "x86_64", "build_type": "Release", "compiler.runtime": "MDd"}.items() + compiler.items()))
-    builder.add(dict({"arch": "x86_64", "build_type": "Debug", "compiler.runtime": "MD"}.items() + compiler.items()))
+    builder.add(dict({"arch": "x86", "build_type": "Release", "compiler.runtime": "MD"}.items() + compiler.items()))
+    builder.add(dict({"arch": "x86", "build_type": "Debug", "compiler.runtime": "MDd"}.items() + compiler.items()))
+    builder.add(dict({"arch": "x86_64", "build_type": "Release", "compiler.runtime": "MD"}.items() + compiler.items()))
+    builder.add(dict({"arch": "x86_64", "build_type": "Debug", "compiler.runtime": "MDd"}.items() + compiler.items()))
     
     builder.run()
