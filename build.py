004d90609a548aa0b03dc7207f19caa8e08fbe79
selenorks <fimochkin@gmail.com>
2017-01-25 14:27:14 +0700

    added debug builds

--- build.py
+++ build.py
@@ -10,8 +10,13 @@ if __name__ == "__main__":
             if settings["compiler"] == "Visual Studio" and settings["compiler.version"] == "14" and settings["compiler.runtime"].startswith("MD"):
                  filtered_builds.append([settings, options])
         builder.builds = filtered_builds
-        
+
     if platform.system() == "Linux":
-        builder.add({"arch": "x86_64", "build_type": "Release", "compiler.libcxx": "libstdc++", 'compiler.version': '4.8', 'compiler': 'gcc'})
-        builder.add({"arch": "x86", "build_type": "Release", "compiler.libcxx": "libstdc++", 'compiler.version': '4.8', 'compiler': 'gcc'})
+        builder.add_common_builds()
+        filtered_builds = []
+        for settings, options in builder.builds:
+            if settings["compiler"] == "gcc":
+                 filtered_builds.append([settings, options])
+        builder.builds = filtered_builds
+
     builder.run()
