5acadde7b6b4a454e59d4fb3c0655bc5f98b5667
Evgeny Fimochkin <evgeny.fimochkin@neulion.com>
2017-01-31 11:48:16 +0700

    fixed build on iOS

--- conanfile.py
+++ conanfile.py
@@ -38,19 +38,19 @@ class JeMallocConan(ConanFile):
             opt = "--host=arm-apple-darwin"
             sdk = "iphoneos"
             arch = self.settings.arch if self.settings.arch != "armv8" else "arm64"
-            host_flags = "-arch %s -miphoneos-version-min=5.0 -isysroot $(xcrun -sdk %s --show-sdk-path)" % (arch, sdk)
+            platform_define =  "__arm__" if self.settings.arch != "armv8" else  "__arm64__"
+            host_flags = "-arch %s -miphoneos-version-min=5.0 -isysroot $(xcrun -sdk %s --show-sdk-path) -I$(xcrun -sdk %s --show-sdk-path) -D%s" % (arch, sdk, platform_define)
             flags = " -O3 -g " if str(self.info.settings.build_type) == "Release" else "-O0 -g "
             exports = [ "HOST_FLAGS=\"%s\"" % host_flags,
                 "CHOST=\"arm-apple-darwin\"",
                 "CC=\"$(xcrun -find -sdk %s clang)\"" % (sdk),
                 "CXX=\"$(xcrun -find -sdk %s clang++)\"" % (sdk),
-                "CPP=\"$(xcrun -find -sdk %s cpp)\"" % (sdk),
                 "LDFLAGS=\"%s\"" % host_flags,
                 "CXXFLAGS=\"%s %s\"" % (host_flags, flags),
                 "CFLAGS=\"%s %s\"" % (host_flags, flags)
                 ]
 
-            self.run("cd %s && %s ./configure %s --disable-libxml2" % (self.ZIP_FOLDER_NAME, " ".join(exports), opt))
+            self.run("cd %s && %s ./autogen.sh %s" % (self.ZIP_FOLDER_NAME, " ".join(exports), opt))
             self.run("cd %s && %s make" % (self.ZIP_FOLDER_NAME, " ".join(exports)))
 
         else:
