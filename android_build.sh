60425863e30f44db22218af3b172382ec1a7c0ce
Evgeny Fimochkin <evgeny.fimochkin@neulion.com>
2017-02-03 11:06:22 +0700

    fix android

--- android_build.sh
+++ android_build.sh
@@ -20,9 +20,14 @@ else
 #armv7
     export PLATFORM=android-16
     export PATH=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin:$PATH
-    export CFLAGS="-fPIC -march=armv7-a -mfloat-abi=softfp -fdiagnostics-color=always -mfpu=neon -mthumb -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 -funwind-tables --sysroot=${ANDROID_NDK}/platforms/${PLATFORM}/arch-arm $OPT_FLAGS"
+    export CFLAGS="-DANDROID \
+-isystem ${ANDROID_NDK}/platforms/${PLATFORM}/arch-arm/usr/include \
+-isystem ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.9/include \
+-isystem ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi-v7a/include \
+-isystem ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.9/include/backward \
+-fexceptions -Wno-psabi --sysroot=${ANDROID_NDK}/platforms/${PLATFORM}/arch-arm -funwind-tables -finline-limit=64 -fsigned-char -no-canonical-prefixes -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fdata-sections -ffunction-sections -Wa,--noexecstack  -Wl,--fix-cortex-a8 -Wl,--no-undefined -Wl,--gc-sections -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -Wl,-z,nocopyreloc -fdiagnostics-color=always -mthumb -fomit-frame-pointer -fno-strict-aliasing -fPIC $OPT_FLAGS"
     export CXXFLAGS="${CFLAGS}"
-    export LDFLAGS="-fPIC -L${ANDROID_NDK}/platforms/${PLATFORM}/arch-arm/usr/lib"
+    export LDFLAGS="-fPIC -L${ANDROID_NDK}/platforms/${PLATFORM}/arch-arm/usr/lib --sysroot=${ANDROID_NDK}/platforms/${PLATFORM}/arch-arm"
     export LIBS="${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi-v7a/libgnustl_shared.so"
     export CC="${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-gcc"
     export CXX="${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-g++"
